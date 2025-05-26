e6a54cff3d69404b8fa78378c8a1fbb3c2f6da89
HALE <haithanh0215@gmail.com>
2025-05-27 02:23:27 +0700

    goto localhost

--- tests/test_index_html.py
+++ tests/test_index_html.py
@@ -5,9 +5,10 @@ def test_open_index_html():
         browser = p.chromium.launch(headless=True)  # Headless cho CI
         page = browser.new_page()
         
-        # Mở file index.html cục bộ
-        page.goto("file:///F:/python_playwright/vscode/CICD_test/cubeit/index.html")
-        
+        # # Mở file index.html cục bộ
+        # page.goto("file:///F:/python_playwright/vscode/CICD_test/cubeit/index.html")
+        page.goto("http://127.0.0.1:8000/")
+
         # Kiểm tra tiêu đề (giả sử index.html có <h1>)
         title = page.locator("h1")
         expect(title).to_have_text("Chào mừng đến với trang CI Test!")
